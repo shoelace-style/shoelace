75e4582dd6820e0cd0f786a5c65ea0452b095f00
Cory LaViska <cory@abeautifulsite.net>
2017-08-30 17:56:41 -0400

    Fixed dropdown trigger bug introduced in #49

--- source/js/dropdowns.js
+++ source/js/dropdowns.js
@@ -37,16 +37,13 @@
   (typeof jQuery === 'function' ? jQuery : Zepto)(function($) {
     $(document)
       .on('click', function(event) {
-        var dropdown;
-        var menu;
-        var selectedItem;
-        var trigger;
+        var dropdown = $(event.target).closest('.dropdown').get(0);
+        var trigger = dropdown ? $(event.target).closest('.dropdown-trigger').get(0) : null;
+        var menu = dropdown ? $(event.target).closest('.dropdown-menu').get(0) : null;
+        var selectedItem = menu ? $(event.target).closest('a').get(0) : null;
 
         // Watch for clicks on dropdown triggers
-        if($(event.target).closest('.dropdown-trigger')) {
-          dropdown = $(event.target).closest('.dropdown');
-          trigger = event.target;
-
+        if(trigger) {
           // Close other dropdowns
           $('.dropdown.active')
             .not(dropdown)
@@ -62,28 +59,31 @@
           $(dropdown)
             .toggleClass('active')
             .trigger($(dropdown).is('.active') ? 'show' : 'hide');
-        } else {
-          menu = $(event.target).closest('.dropdown-menu');
 
-          // Watch for clicks on menu items
-          if(menu.length) {
-            dropdown = $(event.target).closest('.dropdown');
-            selectedItem = $(event.target).closest('a').get(0);
+          return;
+        }
 
-            // If the user selected a menu item and it's not disabled, fire the select event
-            if(selectedItem && !$(selectedItem).is('.disabled')) {
-              $(dropdown).trigger('select', selectedItem);
-            }
-
-            // Prevent the page from scrolling since menu items are #links
-            event.preventDefault();
-          }
-
-          // Close dropdowns on all other clicks
-          $('.dropdown.active')
+        // If the user selected a menu item, close the dropdown and fire the select event
+        if(selectedItem) {
+          $(dropdown)
             .removeClass('active')
             .trigger('hide');
+
+          // Don't select disabled menu items
+          if(!$(selectedItem).is('.disabled')) {
+            $(dropdown).trigger('select', selectedItem);
+          }
+
+          // Prevent the page from scrolling since menu items are #links
+          event.preventDefault();
+
+          return;
         }
+
+        // If the user didn't click a trigger or a menu item, hide any active dropdowns
+        $('.dropdown.active')
+          .removeClass('active')
+          .trigger('hide');
       })
       .on('keydown', function(event) {
         // Close dropdowns on escape
