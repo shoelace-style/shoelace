7553516f1006f154c3de11ac24bc613f41063ac1
Cory LaViska <cory@abeautifulsite.net>
2017-10-10 20:58:00 -0400

    1.0.0-beta24

--- dist/shoelace.js
+++ dist/shoelace.js
@@ -1,12 +1,12 @@
 /*!
-  Shoelace.css dropdowns 1.0.0-beta23
+  Shoelace.css dropdowns 1.0.0-beta24
   (c) A Beautiful Site, LLC
 
   Released under the MIT license
   Source: https://github.com/claviska/shoelace-css
 */
 !function(){"use strict";if("undefined"==typeof jQuery&&"undefined"==typeof Zepto)throw new Error("Shoelace dropdowns require either jQuery or Zepto.");("function"==typeof jQuery?jQuery:Zepto)(function(e){e(document).on("click",function(t){var i=e(t.target).closest(".dropdown").get(0),r=i?e(t.target).closest(".dropdown-trigger").get(0):null,o=(i?e(t.target).closest(".dropdown-menu").get(0):null)?e(t.target).closest("a").get(0):null;if(r){if(e(".dropdown.active").not(i).removeClass("active").trigger("hide"),e(r).is(".disabled, :disabled"))return;e(i).toggleClass("active").trigger(e(i).is(".active")?"show":"hide")}else{if(o)return e(o).is(".disabled")?void t.preventDefault():void e(i).removeClass("active").trigger("hide").trigger(e.Event("select",t),o);i||e(".dropdown.active").removeClass("active").trigger("hide")}}).on("keydown",function(t){27===t.keyCode&&e(".dropdown.active").removeClass("active").trigger("hide")})})}(),/*!
-  Shoelace.css tabs 1.0.0-beta23
+  Shoelace.css tabs 1.0.0-beta24
   (c) A Beautiful Site, LLC
 
   Released under the MIT license
