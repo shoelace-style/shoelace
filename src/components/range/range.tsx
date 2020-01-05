72a51d34a0907bfa6992df707f5d7168b354a88f
Cory LaViska <cory@abeautifulsite.net>
2020-01-05 14:09:46 -0500

    Initial range commit

---
+++ src/components/range/range.tsx
@@ -0,0 +1,72 @@
+import { Component, Method, Prop, State, h } from '@stencil/core';
+
+@Component({
+  tag: 's-range',
+  styleUrl: 'range.scss',
+  scoped: true
+})
+export class Range {
+  input: HTMLInputElement;
+
+  @State() hasFocus = false;
+
+  /** The input's name attribute. */
+  @Prop() name = '';
+
+  /** The input's value attribute. */
+  @Prop({ mutable: true }) value = '';
+
+  /** Set to true to disable the input. */
+  @Prop() disabled = false;
+
+  /** The input's min attribute. */
+  @Prop() min: number;
+
+  /** The input's max attribute. */
+  @Prop() max: number;
+
+  /** The input's step attribute. */
+  @Prop() step: number;
+
+  /** Sets focus on the input. */
+  @Method()
+  async setFocus() {
+    this.input.focus();
+  }
+
+  /** Removes focus from the input. */
+  @Method()
+  async removeFocus() {
+    this.input.blur();
+  }
+
+  render() {
+    return (
+      <div
+        class={{
+          's-range': true,
+
+          // States
+          's-range--disabled': this.disabled,
+          's-range--focused': this.hasFocus
+        }}
+        onClick={() => this.input.focus()}
+      >
+        <input
+          ref={el => (this.input = el)}
+          type="range"
+          class="s-range__control"
+          name={this.name}
+          disabled={this.disabled}
+          min={this.min}
+          max={this.max}
+          step={this.step}
+          value={this.value}
+          onFocus={() => (this.hasFocus = true)}
+          onBlur={() => (this.hasFocus = false)}
+          onInput={() => (this.value = this.input.value)}
+        />
+      </div>
+    );
+  }
+}
