f0b3ce08c5ded8ecfc739c49ee743fdcb0e50d35
Cory LaViska <cory@abeautifulsite.net>
2020-02-04 23:07:45 -0500

    Add SVG icons

--- src/utilities/offset.ts
+++ src/utilities/offset.ts
@@ -2,8 +2,8 @@
 // Returns an element's offset relative to its parent. Similar to element.offsetTop and element.offsetLeft, except the
 // parent doesn't have to be positioned relative or absolute.
 //
-// NOTE: This was created to work around what appears to be a bug in Firefox where a slotted element's offsetParent
-// seems to ignore elements inside the surrounding shadow DOM: https://bugzilla.mozilla.org/show_bug.cgi?id=1611429
+// NOTE: This was created to work around what appears to be a bug in Chrome where a slotted element's offsetParent
+// seems to ignore elements inside the surrounding shadow DOM: https://bugs.chromium.org/p/chromium/issues/detail?id=920069
 //
 export function getOffset(element: HTMLElement, parent: HTMLElement) {
   return {
