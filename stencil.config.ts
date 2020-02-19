b4f19b77707a525d87c02586bda68be8ddba7f71
Cory LaViska <cory@abeautifulsite.net>
2020-02-18 20:56:49 -0500

    Use uppercase namespace

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
-  namespace: 'shoelace',
+  namespace: 'Shoelace',
   outputTargets: [
     {
       type: 'dist',
