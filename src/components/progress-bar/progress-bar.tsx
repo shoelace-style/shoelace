56afad80c66ab3925bebade8d02d9a874b432378
Cory LaViska <cory@abeautifulsite.net>
2020-02-26 23:02:52 -0500

    Add height

--- src/components/progress-bar/progress-bar.tsx
+++ src/components/progress-bar/progress-bar.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, Host, Prop, h } from '@stencil/core';
 
 /**
  * @slot - A label to show inside the indicator.
@@ -13,24 +13,35 @@ export class ProgressBar {
   /** The progress bar's percentage, 0 to 100. */
   @Prop() percentage = 0;
 
+  /** The height of the progress bar in pixels. */
+  @Prop() height = 18;
+
   render() {
     return (
-      <div
-        class="sl-progress-bar"
-        role="progressbar"
-        aria-valuemin="0"
-        aria-valuemax="100"
-        aria-valuenow={this.percentage}
+      <Host
+        style={{
+          '--height': `${this.height}px`
+        }}
       >
         <div
-          class="sl-progress-bar__indicator"
-          style={{
-            width: `${this.percentage}%`
-          }}
+          class="sl-progress-bar"
+          role="progressbar"
+          aria-valuemin="0"
+          aria-valuemax="100"
+          aria-valuenow={this.percentage}
         >
-          <slot />
+          <div
+            class="sl-progress-bar__indicator"
+            style={{
+              width: `${this.percentage}%`
+            }}
+          >
+            <span class="sl-progress-bar__label">
+              <slot />
+            </span>
+          </div>
         </div>
-      </div>
+      </Host>
     );
   }
 }
--- src/components/progress-bar/readme.md
+++ src/components/progress-bar/readme.md
@@ -7,9 +7,10 @@
 
 ## Properties
 
-| Property     | Attribute    | Description                              | Type     | Default |
-| ------------ | ------------ | ---------------------------------------- | -------- | ------- |
-| `percentage` | `percentage` | The progress bar's percentage, 0 to 100. | `number` | `0`     |
+| Property     | Attribute    | Description                               | Type     | Default |
+| ------------ | ------------ | ----------------------------------------- | -------- | ------- |
+| `height`     | `height`     | The height of the progress bar in pixels. | `number` | `18`    |
+| `percentage` | `percentage` | The progress bar's percentage, 0 to 100.  | `number` | `0`     |
 
 
 ## Slots
