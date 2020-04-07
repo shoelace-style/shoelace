7323f4e374a1896ae6c840976494856aabe5f4f2
Cory LaViska <cory@abeautifulsite.net>
2020-04-07 12:45:38 -0400

    Copy license too

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,6 @@
 const copy = require('recursive-copy');
 
-copy('./node_modules/bootstrap-icons/icons', './src/components/icon/icons', { overwrite: true }).catch(err =>
-  console.error(err)
-);
+Promise.all([
+  copy('./node_modules/bootstrap-icons/icons', './src/components/icon/icons', { overwrite: true }),
+  copy('./node_modules/bootstrap-icons/LICENSE.md', './src/components/icon/icons/LICENSE.md', { overwrite: true })
+]).catch(err => console.error(err));
