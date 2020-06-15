725c85a01730399390cc58066a416bab3846a560
Cory LaViska <cory@abeautifulsite.net>
2020-06-15 16:24:30 -0400

    Add slot utiliities

---
+++ src/utilities/slot.ts
@@ -0,0 +1,37 @@
+//
+// Given a slot, this function iterates over all of its assigned element and text nodes and returns the concatenated
+// HTML as a string. This is useful because we can't use slot.innerHTML as an alternative.
+//
+export function getInnerHTML(slot: HTMLSlotElement): string {
+  const nodes = slot.assignedNodes({ flatten: true });
+  let html = '';
+
+  [...nodes].map(node => {
+    if (node.nodeType === Node.ELEMENT_NODE) {
+      html += (node as HTMLElement).outerHTML;
+    }
+
+    if (node.nodeType === Node.TEXT_NODE) {
+      html += node.textContent;
+    }
+  });
+
+  return html;
+}
+
+//
+// Given a slot, this function iterates over all of its assigned text nodes and returns the concatenated text as a
+// string. This is useful because we can't use slot.textContent as an alternative.
+//
+export function getTextContent(slot: HTMLSlotElement): string {
+  const nodes = slot.assignedNodes({ flatten: true });
+  let text = '';
+
+  [...nodes].map(node => {
+    if (node.nodeType === Node.TEXT_NODE) {
+      text += node.textContent;
+    }
+  });
+
+  return text;
+}
