5e8408be0e2f6bcc071566e442a8be8c64d7945d
Cory LaViska <cory@abeautifulsite.net>
2020-06-18 17:04:31 -0400

    Validate SVGs

--- src/components/icon/request.ts
+++ src/components/icon/request.ts
@@ -5,20 +5,22 @@ export const requestIcon = (url: string) => {
   let req = requests.get(url);
 
   if (!req) {
-    req = fetch(url)
-      .then(async res => {
-        if (res.ok) {
-          const svg = await res.text();
-          cache.set(url, svg);
-          return svg;
+    req = fetch(url).then(async res => {
+      if (res.ok) {
+        const div = document.createElement('div');
+        div.innerHTML = await res.text();
+
+        const svg = div.firstElementChild;
+        if (svg && svg.tagName.toLowerCase() === 'svg') {
+          cache.set(url, div.innerHTML);
+          return svg.outerHTML;
         } else {
-          cache.set(url, '');
           return '';
         }
-      })
-      .catch(() => {
-        cache.set(url, '');
-      });
+      } else {
+        return '';
+      }
+    });
 
     requests.set(url, req);
   }
