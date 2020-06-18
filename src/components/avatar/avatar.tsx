3860a5ac84beaa75034f655a46810a1e772fdf86
Cory LaViska <cory@abeautifulsite.net>
2020-06-18 17:27:42 -0400

    Use filled icon as default

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -49,7 +49,7 @@ export class Avatar {
         {!this.initials && (
           <div class="sl-avatar__icon">
             <slot name="icon">
-              <sl-icon name="person" />
+              <sl-icon name="person-fill" />
             </slot>
           </div>
         )}
