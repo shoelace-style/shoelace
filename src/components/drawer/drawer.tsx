6b79ac68ec61fe6a4676a757ae0e1579e73726fa
Cory LaViska <cory@abeautifulsite.net>
2020-06-24 08:04:25 -0400

    Increase font size of close icon

--- src/components/drawer/drawer.tsx
+++ src/components/drawer/drawer.tsx
@@ -1,77 +1,87 @@
-import { Component, Element, Event, EventEmitter, Method, Prop, Watch, h } from '@stencil/core';
+import { Component, Element, Event, EventEmitter, Host, Method, Prop, Watch, h } from '@stencil/core';
+import { lockBodyScrolling, unlockBodyScrolling } from '../../utilities/scroll';
 import { focusVisible } from '../../utilities/focus-visible';
+
+let id = 0;
 
 /**
  * @since 1.0.0
  * @status ready
+ *
+ * @slot - The drawer's content.
+ * @slot footer - The drawer's footer, usually one or more buttons representing various options.
  */
-
 @Component({
   tag: 'sl-drawer',
   styleUrl: 'drawer.scss',
   shadow: true
 })
 export class Drawer {
+  panel: HTMLElement;
   drawer: HTMLElement;
+  id = `drawer-${++id}`;
 
   constructor() {
+    this.keepdrawerFocused = this.keepdrawerFocused.bind(this);
     this.handleCloseClick = this.handleCloseClick.bind(this);
+    this.handleTransitionEnd = this.handleTransitionEnd.bind(this);
     this.handleDocumentKeyDown = this.handleDocumentKeyDown.bind(this);
     this.handleDocumentMouseDown = this.handleDocumentMouseDown.bind(this);
-    this.handleTransitionEnd = this.handleTransitionEnd.bind(this);
+    this.handleOverlayClick = this.handleOverlayClick.bind(this);
   }
 
   @Element() host: HTMLSlDrawerElement;
 
-  /** The location from which the drawer will slide out. */
-  @Prop() placement: 'left' | 'right' = 'right';
+  /** Indicates whether or not the drawer is open. You can use this in lieu of the show/hide methods. */
+  @Prop({ mutable: true, reflect: true }) open = false;
 
   /**
-   * The drawer's position. Use `fixed` to make the drawer slide out from the viewport. To make the drawer slide out
-   * of an arbitrary element, use `absolute` and place the drawer inside a `position: relative` container and set
-   * `overflow: hidden` on it.
-   */
-  @Prop() position: 'absolute' | 'fixed' = 'fixed';
-
-  /**
-   * The dialog's label as displayed in the header. You should always include a relevant label even when using
+   * The drawer's label as displayed in the header. You should always include a relevant label even when using
    * `no-header`, as it is required for proper accessibility.
    */
   @Prop() label = '';
 
-  /**
-   * Prevents the drawer from closing automatically when the user interacts outside of it (e.g. clicks something outside
-   * of it).
-   */
+  /** The direction from which the drawer will open. */
+  @Prop() placement: 'left' | 'right' = 'right';
+
+  /** When true, the drawer will not be dismissed when the user clicks outside of it. */
   @Prop() pinned = false;
 
+  /** Adds an overlay when the drawer is open. */
+  @Prop() overlay = false;
+
   /**
-   * Set to true to disable the header. This will also remove the default close button, so please ensure you provide an
-   * easy, accessible way for users to dismiss the dialog.
+   * Removes the header. This will also remove the default close button, so please ensure you provide an easy,
+   * accessible way for users to dismiss the drawer.
    */
   @Prop() noHeader = false;
 
-  /** Set to true to disable the footer. */
+  /** Removes the footer. */
   @Prop() noFooter = false;
-
-  /** Indicates whether or not the dialog is open. You can use this in lieu of the show/hide methods. */
-  @Prop({ mutable: true, reflect: true }) open = false;
 
   @Watch('open')
   handleOpenChange() {
     this.open ? this.show() : this.hide();
   }
 
-  /** Emitted when the dialog opens. Calling `event.preventDefault()` will prevent it from being opened. */
+  @Watch('overlay')
+  handleOverlayChange() {
+    // Body scrolling should only be locked when the drawer is open and the overlay is enabled
+    if (this.open) {
+      this.overlay ? lockBodyScrolling(this.host) : unlockBodyScrolling(this.host);
+    }
+  }
+
+  /** Emitted when the drawer opens. Calling `event.preventDefault()` will prevent it from being opened. */
   @Event() slShow: EventEmitter;
 
-  /** Emitted after the dialog opens and all transitions are complete. */
+  /** Emitted after the drawer opens and all transitions are complete. */
   @Event() slAfterShow: EventEmitter;
 
-  /** Emitted when the dialog closes. Calling `event.preventDefault()` will prevent it from being closed. */
+  /** Emitted when the drawer closes. Calling `event.preventDefault()` will prevent it from being closed. */
   @Event() slHide: EventEmitter;
 
-  /** Emitted after the dialog closes and all transitions are complete. */
+  /** Emitted after the drawer closes and all transitions are complete. */
   @Event() slAfterHide: EventEmitter;
 
   componentDidLoad() {
@@ -85,9 +95,10 @@ export class Drawer {
 
   componentDidUnload() {
     focusVisible.unobserve(this.drawer);
+    unlockBodyScrolling(this.host);
   }
 
-  /** Shows the dialog */
+  /** Shows the drawer */
   @Method()
   async show() {
     const slShow = this.slShow.emit();
@@ -99,12 +110,18 @@ export class Drawer {
     this.host.hidden = false;
     this.host.clientWidth; // force a reflow
     this.open = true;
+    this.panel.focus();
 
+    if (this.overlay) {
+      lockBodyScrolling(this.host);
+    }
+
+    document.addEventListener('focusin', this.keepdrawerFocused);
     document.addEventListener('keydown', this.handleDocumentKeyDown);
     document.addEventListener('mousedown', this.handleDocumentMouseDown);
   }
 
-  /** Hides the dialog */
+  /** Hides the drawer */
   @Method()
   async hide() {
     const slHide = this.slHide.emit();
@@ -115,8 +132,13 @@ export class Drawer {
 
     this.open = false;
 
+    if (this.overlay) {
+      unlockBodyScrolling(this.host);
+    }
+
+    document.removeEventListener('focusin', this.keepdrawerFocused);
     document.removeEventListener('keydown', this.handleDocumentKeyDown);
-    document.addEventListener('mousedown', this.handleDocumentMouseDown);
+    document.removeEventListener('mousedown', this.handleDocumentMouseDown);
   }
 
   handleCloseClick() {
@@ -131,9 +153,13 @@ export class Drawer {
 
   handleDocumentMouseDown(event: MouseEvent) {
     const target = event.target as HTMLElement;
-
-    // Close when clicking outside of the drawer
     if (!this.pinned && target.closest('sl-drawer') !== this.host) {
+      this.hide();
+    }
+  }
+
+  handleOverlayClick() {
+    if (!this.pinned) {
       this.hide();
     }
   }
@@ -142,53 +168,69 @@ export class Drawer {
     const target = event.target as HTMLElement;
 
     // Ensure we only handle one transition event on the target element
-    if (event.propertyName === 'transform' && target.classList.contains('drawer')) {
+    if (event.propertyName === 'transform' && target.classList.contains('drawer__panel')) {
       this.host.hidden = !this.open;
       this.open ? this.slAfterShow.emit() : this.slAfterHide.emit();
     }
   }
 
+  keepdrawerFocused(event: Event) {
+    const target = event.target as HTMLElement;
+
+    if (!target.closest('sl-drawer')) {
+      this.panel.focus();
+    }
+  }
+
   render() {
     return (
-      <div
-        ref={el => (this.drawer = el)}
-        class={{
-          drawer: true,
-          'drawer--open': this.open,
+      <Host hidden>
+        <div
+          ref={el => (this.drawer = el)}
+          class={{
+            drawer: true,
+            'drawer--open': this.open,
+            'drawer--left': this.placement === 'left',
+            'drawer--right': this.placement === 'right'
+          }}
+          onTransitionEnd={this.handleTransitionEnd}
+        >
+          <div
+            ref={el => (this.panel = el)}
+            class="drawer__panel"
+            role="drawer"
+            aria-modal="true"
+            aria-hidden={!this.open}
+            aria-label={this.noHeader ? this.label : null}
+            aria-labeledby={!this.noHeader ? `${this.id}-title` : null}
+            tabIndex={0}
+          >
+            {!this.noHeader && (
+              <header class="drawer__header">
+                <span class="drawer__title" id={`${this.id}-title`}>
+                  {/* If there's no label, use an invisible character to prevent the heading from collapsing */}
+                  {this.label || String.fromCharCode(65279)}
+                </span>
+                <button class="drawer__close" type="button" onClick={this.handleCloseClick}>
+                  <sl-icon name="x"></sl-icon>
+                </button>
+              </header>
+            )}
 
-          // Position
-          'drawer--absolute': this.position === 'absolute',
-          'drawer--fixed': this.position === 'fixed',
+            <div class="drawer__body">
+              <slot />
+            </div>
 
-          // Placement
-          'drawer--left': this.placement === 'left',
-          'drawer--right': this.placement === 'right'
-        }}
-        aria-hidden={!this.open}
-        onTransitionEnd={this.handleTransitionEnd}
-      >
-        {!this.noHeader && (
-          <header class="drawer__header">
-            <span class="drawer__title">
-              {/* If there's no label, use an invisible character to prevent the heading from collapsing */}
-              {this.label || String.fromCharCode(65279)}
-            </span>
-            <button class="drawer__close" type="button" onClick={this.handleCloseClick}>
-              <sl-icon name="x"></sl-icon>
-            </button>
-          </header>
-        )}
+            {!this.noFooter && (
+              <footer class="drawer__footer">
+                <slot name="footer" />
+              </footer>
+            )}
+          </div>
 
-        <div class="drawer__body">
-          <slot />
+          {this.overlay && <div class="drawer__overlay" onClick={this.handleOverlayClick} />}
         </div>
-
-        {!this.noFooter && (
-          <footer class="drawer__footer">
-            <slot name="footer" />
-          </footer>
-        )}
-      </div>
+      </Host>
     );
   }
 }
