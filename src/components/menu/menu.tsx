23dc3cd96462e71d15b983f5913e07a7527415f0
Cory LaViska <cory@abeautifulsite.net>
2020-07-01 11:09:56 -0400

    Don't cycle through options

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -136,8 +136,8 @@ export class Menu {
           index = items.length - 1;
         }
 
-        if (index < 0) index = items.length - 1;
-        if (index > items.length - 1) index = 0;
+        if (index < 0) index = 0;
+        if (index > items.length - 1) index = items.length - 1;
 
         this.setActiveItem(items[index]);
         this.scrollItemIntoView(items[index]);
