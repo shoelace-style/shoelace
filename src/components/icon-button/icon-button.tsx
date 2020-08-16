2ffc1de1b6fe0fcafd25d596a00a56d53396875c
Cory LaViska <cory@abeautifulsite.net>
2020-08-16 11:24:43 -0400

    Reflect icon button props

--- src/components/icon-button/icon-button.tsx
+++ src/components/icon-button/icon-button.tsx
@@ -17,16 +17,16 @@ export class IconButton {
   button: HTMLButtonElement;
 
   /** The name of the icon to draw. See the icon component for a full list of icons. */
-  @Prop() name: string;
+  @Prop({ reflect: true }) name: string;
 
   /** An external URL of an SVG file. */
-  @Prop() src: string;
+  @Prop({ reflect: true }) src: string;
 
   /** An alternative description to use for accessibility. If omitted, the name or src will be used to generate it. */
-  @Prop() label: string;
+  @Prop({ reflect: true }) label: string;
 
   /** Set to true to disable the button. */
-  @Prop() disabled = false;
+  @Prop({ reflect: true }) disabled = false;
 
   componentDidLoad() {
     focusVisible.observe(this.button);
