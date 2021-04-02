d59fdbc443da898ce1e0986a6fe85d8405187715
Cory LaViska <cory@abeautifulsite.net>
2021-04-02 14:42:01 -0400

    remove unused import

--- src/components/details/details.ts
+++ src/components/details/details.ts
@@ -1,7 +1,6 @@
 import { LitElement, html, unsafeCSS } from 'lit';
 import { customElement, property, query } from 'lit/decorators';
 import { classMap } from 'lit-html/directives/class-map';
-import { ifDefined } from 'lit-html/directives/if-defined';
 import { event, EventEmitter, watch } from '../../internal/decorators';
 import styles from 'sass:./details.scss';
 import { focusVisible } from '../../internal/focus-visible';
