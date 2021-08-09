abaf053250129d41c93196b659fdf44286221880
Benny Powers <web@bennypowers.com>
2021-08-09 23:54:09 +0300

    feat: add href and friends to icon-button
    closes #495

--- src/components/icon-button/icon-button.ts
+++ src/components/icon-button/icon-button.ts
@@ -19,7 +19,7 @@ import '../icon/icon';
 export default class SlIconButton extends LitElement {
   static styles = styles;
 
-  @query('button') button: HTMLButtonElement;
+  @query('button') button: HTMLButtonElement|HTMLLinkElement;
 
   /** The name of the icon to draw. */
   @property() name: string;
@@ -29,6 +29,15 @@ export default class SlIconButton extends LitElement {
 
   /** An external URL of an SVG file. */
   @property() src: string;
+
+  /** When set, the underlying button will be rendered as an `<a>` with this `href` instead of a `<button>`. */
+  @property() href: string;
+
+  /** Tells the browser where to open the link. Only used when `href` is set. */
+  @property() target: '_blank' | '_parent' | '_self' | '_top';
+
+  /** Tells the browser to download the linked file as this filename. Only used when `href` is set. */
+  @property() download: string;
 
   /**
    * A description that gets read by screen readers and other assistive devices. For optimal accessibility, you should
@@ -50,7 +59,34 @@ export default class SlIconButton extends LitElement {
   }
 
   render() {
-    return html`
+    const isLink = this.href ? true : false;
+
+    const interior = html`
+      <sl-icon
+        name=${ifDefined(this.name)}
+        library=${ifDefined(this.library)}
+        src=${ifDefined(this.src)}
+        aria-hidden="true"
+      ></sl-icon>
+    `;
+
+    return isLink ? html`
+    <a
+      ref=${(el: HTMLLinkElement) => (this.button = el)}
+      part="base"
+      class="icon-button"
+      href=${ifDefined(this.href)}
+      target=${ifDefined(this.target)}
+      download=${ifDefined(this.download)}
+      rel=${ifDefined(this.target ? 'noreferrer noopener' : undefined)}
+      role="button"
+      aria-disabled=${this.disabled ? 'true' : 'false'}
+      aria-label="${this.label}"
+      tabindex=${this.disabled ? '-1' : '0'}
+    >
+      ${interior}
+    </a>
+    ` : html`
       <button
         part="base"
         class=${classMap({
@@ -61,12 +97,7 @@ export default class SlIconButton extends LitElement {
         type="button"
         aria-label=${this.label}
       >
-        <sl-icon
-          name=${ifDefined(this.name)}
-          library=${ifDefined(this.library)}
-          src=${ifDefined(this.src)}
-          aria-hidden="true"
-        ></sl-icon>
+        ${interior}
       </button>
     `;
   }
