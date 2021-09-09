ba3117f435d6fbb0f30188de94d102cbc50618e6
Cory LaViska <cory@abeautifulsite.net>
2021-09-09 09:13:29 -0400

    fix comment

--- src/components/tooltip/tooltip.ts
+++ src/components/tooltip/tooltip.ts
@@ -20,7 +20,7 @@ let id = 0;
  * @event sl-show - Emitted when the tooltip begins to show.
  * @event sl-after-show - Emitted after the tooltip has shown and all transitions are complete.
  * @event sl-hide - Emitted when the tooltip begins to hide.
- * @event sl-after-hide - Emitted after the tooltip has hidden and all transitions are complete. *
+ * @event sl-after-hide - Emitted after the tooltip has hidden and all transitions are complete.
  *
  * @csspart base - The component's base wrapper.
  *
