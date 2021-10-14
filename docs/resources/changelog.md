8dba8fa5fbbec611561de22e0f13d19c45b8f3ec
Cory LaViska <cory@abeautifulsite.net>
2021-10-14 09:01:37 -0400

    fix tooltip bug

--- docs/resources/changelog.md
+++ docs/resources/changelog.md
@@ -9,6 +9,7 @@ _During the beta period, these restrictions may be relaxed in the event of a mis
 ## Next
 
 - Added `label` attribute to `<sl-progress-bar>` and `<sl-progress-ring>` to improve a11y
+- Fixed a bug where the tooltip would show briefly when clicking a disabled `<sl-range>`
 - Updated to Bootstrap Icons to 1.6.0
 
 ## 2.0.0-beta.57
--- src/components/range/range.ts
+++ src/components/range/range.ts
@@ -247,7 +247,7 @@ export default class SlRange extends LitElement {
             @focus=${this.handleFocus}
             @blur=${this.handleBlur}
           />
-          ${this.tooltip !== 'none'
+          ${this.tooltip !== 'none' && !this.disabled
             ? html` <output part="tooltip" class="range__tooltip"> ${this.tooltipFormatter(this.value)} </output> `
             : ''}
         </div>
