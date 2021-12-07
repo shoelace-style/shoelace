b32d3e5c17428819cbc32a2ac31f2e6de0132a06
Cory LaViska <cory@abeautifulsite.net>
2021-12-07 10:35:41 -0500

    consistent name/code

--- src/translations/de-ch.ts
+++ src/translations/de-ch.ts
@@ -2,16 +2,16 @@ import { registerTranslation } from '../utilities/localize';
 import type { Translation } from '../utilities/localize';
 
 const translation: Translation = {
-  $code: 'de_CH',
+  $code: 'de-CH',
   $name: 'Deutsch (Schweiz)',
   $dir: 'ltr',
 
-  close: "Schliessen",
+  close: 'Schliessen',
   copy: 'Kopieren',
   progress: 'Fortschritt',
   scroll_to_end: 'Zum Ende scrollen',
   scroll_to_start: 'Zum Anfang scrollen',
-  select_a_color_from_the_screen: "Wähle eine Farbe vom Bildschirm",
+  select_a_color_from_the_screen: 'Wähle eine Farbe vom Bildschirm',
   toggle_color_format: 'Farbformat umschalten'
 };
 
