a2a48415f747fd1985ab82b939e45346dce44e0d
Max <max.roecker@gmail.com>
2021-12-07 10:19:11 -0300

    Add portuguese translations (#605)

---
+++ src/translations/pt.ts
@@ -0,0 +1,20 @@
+import { registerTranslation } from '../utilities/localize';
+import type { Translation } from '../utilities/localize';
+
+const translation: Translation = {
+  $code: 'pt',
+  $name: 'Português',
+  $dir: 'ltr',
+
+  close: 'Fechar',
+  copy: 'Copiar',
+  progress: 'Progresso',
+  scroll_to_end: 'Rolar até o final',
+  scroll_to_start: 'Rolar até o começo',
+  select_a_color_from_the_screen: 'Selecionar uma cor da tela',
+  toggle_color_format: 'Trocar o formato de cor'
+};
+
+registerTranslation(translation);
+
+export default translation;
