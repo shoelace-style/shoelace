033a3a9db9e55a33b0efe4d71496f1300acbd4a1
Cory LaViska <cory@abeautifulsite.net>
2022-02-16 16:02:21 -0500

    finishing touches

--- scripts/make-web-types.js
+++ scripts/make-web-types.js
@@ -1,12 +1,14 @@
 //
-// This script converts the custom-elements.json to generate web-types.json
+// This script generates a web-types.json file from custom-elements.json for use with WebStorm/PHPStorm
+//
+// Docs: https://github.com/JetBrains/web-types
 //
 import commandLineArgs from 'command-line-args';
 import jsonata from 'jsonata';
 import fs from 'fs';
 import path from 'path';
 
-const {outdir} = commandLineArgs({name: 'outdir', type: String});
+const { outdir } = commandLineArgs({ name: 'outdir', type: String });
 const metadata = JSON.parse(fs.readFileSync(path.join(outdir, 'custom-elements.json'), 'utf8'));
 
 const jsonataExprString = `{
@@ -60,9 +62,8 @@ const jsonataExprString = `{
   }
 }`;
 
-
 // Run the conversion
-const expression = jsonata(jsonataExprString)
+const expression = jsonata(jsonataExprString);
 
 console.log('Generating web types');
 fs.writeFileSync(path.join(outdir, 'web-types.json'), JSON.stringify(expression.evaluate(metadata), null, 2), 'utf8');
