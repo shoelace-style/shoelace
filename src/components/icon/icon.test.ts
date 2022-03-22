f676949460acd4e99093d238b5952b9de4a05393
Cory LaViska <cory@abeautifulsite.net>
2022-03-22 11:03:00 -0400

    remove unused import

--- src/components/icon/icon.test.ts
+++ src/components/icon/icon.test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-restricted-imports */
-import { elementUpdated, expect, fixture, html, oneEvent, waitUntil } from '@open-wc/testing';
+import { elementUpdated, expect, fixture, html, oneEvent } from '@open-wc/testing';
 /* @ts-expect-error - TODO - switch to path aliases when Web Test Runner's esbuild plugin allows it */
 import { registerIconLibrary } from '../../../dist/shoelace.js';
 import type SlIcon from './icon';
