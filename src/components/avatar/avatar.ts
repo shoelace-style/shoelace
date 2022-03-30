ee1a0c2c594f5bfe7aa58527ffa579fa46e37571
Cory LaViska <cory@abeautifulsite.net>
2022-03-30 09:05:19 -0400

    fixes #717

--- src/components/avatar/avatar.ts
+++ src/components/avatar/avatar.ts
@@ -2,6 +2,7 @@ import { html, LitElement } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
 import '../../components/icon/icon';
+import { watch } from '../../internal/watch';
 import styles from './avatar.styles';
 
 /**
@@ -26,16 +27,22 @@ export default class SlAvatar extends LitElement {
   @state() private hasError = false;
 
   /** The image source to use for the avatar. */
-  @property() image?: string;
+  @property() image = '';
 
   /** A label to use to describe the avatar to assistive devices. */
   @property() label = '';
 
   /** Initials to use as a fallback when no image is available (1-2 characters max recommended). */
-  @property() initials?: string;
+  @property() initials = '';
 
   /** The shape of the avatar. */
   @property({ reflect: true }) shape: 'circle' | 'square' | 'rounded' = 'circle';
+
+  @watch('image')
+  handleImageChange() {
+    // Reset the error when a new image is provided
+    this.hasError = false;
+  }
 
   render() {
     return html`
@@ -59,7 +66,7 @@ export default class SlAvatar extends LitElement {
                 </slot>
               </div>
             `}
-        ${typeof this.image === 'string' && !this.hasError
+        ${this.image && !this.hasError
           ? html`
               <img
                 part="image"
