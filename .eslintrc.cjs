a5b33342229e911ec4b535ca58407b310ebb4341
Cory LaViska <cory@abeautifulsite.net>
2022-04-13 10:59:24 -0400

    remove rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -180,25 +180,9 @@ module.exports = {
     'prefer-template': 'warn',
     'no-else-return': 'warn',
     'func-names': ['warn', 'never'],
-    'func-style': ['warn', 'declaration'],
     'one-var': ['warn', 'never'],
     'operator-assignment': 'warn',
     'prefer-arrow-callback': 'warn',
-    'no-restricted-syntax': [
-      'warn',
-      {
-        selector: "CallExpression[callee.name='String']",
-        message: "Don't use the String function. Use .toString() instead."
-      },
-      {
-        selector: "CallExpression[callee.name='Number']",
-        message: "Don't use the Number function. Use parseInt or parseFloat instead."
-      },
-      {
-        selector: "CallExpression[callee.name='Boolean']",
-        message: "Don't use the Boolean function. Use a strict comparison instead."
-      }
-    ],
     'no-restricted-imports': [
       'warn',
       {
