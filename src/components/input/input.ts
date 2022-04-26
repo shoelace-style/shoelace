1e2ceb82524c95c072098831f32f979be5327b07
Cory LaViska <cory@abeautifulsite.net>
2022-04-26 08:13:13 -0400

    prettier

--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -56,8 +56,16 @@ export default class SlInput extends LitElement {
   @state() private isPasswordVisible = false;
 
   /** The input's type. */
-  @property({ reflect: true }) type: 'date' | 'email' | 'number' | 'password' | 'search' | 'tel' | 'text' | 'time' | 'url' =
-    'text';
+  @property({ reflect: true }) type:
+    | 'date'
+    | 'email'
+    | 'number'
+    | 'password'
+    | 'search'
+    | 'tel'
+    | 'text'
+    | 'time'
+    | 'url' = 'text';
 
   /** The input's size. */
   @property({ reflect: true }) size: 'small' | 'medium' | 'large' = 'medium';
