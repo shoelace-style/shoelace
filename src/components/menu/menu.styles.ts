4b9e35313d1f95e3f44537f83363a29df20e90ac
Cory LaViska <cory@abeautifulsite.net>
2022-08-10 09:13:50 -0400

    remove dup

--- src/components/menu/menu.styles.ts
+++ src/components/menu/menu.styles.ts
@@ -12,7 +12,6 @@ export default css`
     background: var(--sl-panel-background-color);
     border: solid var(--sl-panel-border-width) var(--sl-panel-border-color);
     border-radius: var(--sl-border-radius-medium);
-    background: var(--sl-panel-background-color);
     padding: var(--sl-spacing-x-small) 0;
   }
 
