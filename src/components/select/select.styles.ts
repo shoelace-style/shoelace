e42beab336a5bca3bddbb84a4e32368a8b1b9c08
Cory LaViska <cory@abeautifulsite.net>
2022-08-24 15:08:32 -0400

    remove extra border and redundant styels

--- src/components/select/select.styles.ts
+++ src/components/select/select.styles.ts
@@ -33,13 +33,7 @@ export default css`
     cursor: pointer;
   }
 
-  .select__menu::part(base) {
-    border: none;
-  }
-
   .select::part(panel) {
-    background: var(--sl-panel-background-color);
-    border: solid var(--sl-panel-border-width) var(--sl-panel-border-color);
     border-radius: var(--sl-border-radius-medium);
   }
 
