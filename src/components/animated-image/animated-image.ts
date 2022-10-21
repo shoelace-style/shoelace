6ceaf9428348cae935deda843fd1ff64b554375c
Manuel Schmidt <m.schmidt@complex-it.de>
2022-10-21 14:47:13 +0200

    remove `title` for further usage of `Sl` classnames in docs

--- src/components/animated-image/animated-image.ts
+++ src/components/animated-image/animated-image.ts
@@ -7,7 +7,6 @@ import styles from './animated-image.styles';
 import type { CSSResultGroup } from 'lit';
 
 /**
- * @title AnimatedImage
  * @summary A component for displaying animated GIFs and WEBPs that play and pause on interaction.
  *
  * @since 2.0
