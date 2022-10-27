67e23d8245d498efcc4be872c7c0b9e896e5dba6
Buni48 <dago24@posteo.de>
2022-10-27 18:47:48 +0200

    show border on active

--- src/components/button/button.styles.ts
+++ src/components/button/button.styles.ts
@@ -558,8 +558,7 @@ export default css`
   }
 
   /* Add a visual separator between solid buttons */
-  :host(.sl-button-group__button:not(.sl-button-group__button--radio, [variant='default']):not(:hover, :active))
-    .button:after {
+  :host(.sl-button-group__button:not(.sl-button-group__button--radio, [variant='default']):not(:hover)) .button:after {
     content: '';
     position: absolute;
     top: 0;
