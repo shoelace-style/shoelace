67fbe3b34ea52945a4ab824799931ade03bd4774
Cory LaViska <cory@abeautifulsite.net>
2023-01-04 09:58:56 -0500

    fixes #1101

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -4,7 +4,7 @@ import { globbySync } from 'globby';
 
 export default {
   rootDir: '.',
-  files: 'src/**/*.test.ts',
+  files: 'src/**/*.test.ts', // "default" group
   concurrentBrowsers: 3,
   nodeResolve: true,
   plugins: [
@@ -27,8 +27,8 @@ export default {
       </body>
     </html>
   `,
-  // Create a named group for every test file to enable running single tests.
-  // If a test file is `split-panel.test.ts` then you can run `npm run test -- --group split-panel` to run only that file's tests
+  // Create a named group for every test file to enable running single tests. If a test file is `split-panel.test.ts`
+  // then you can run `npm run test -- --group split-panel` to run only that component's tests.
   groups: globbySync('src/**/*.test.ts').map(path => {
     const groupName = path.match(/^.*\/(?<fileName>.*)\.test\.ts/).groups.fileName;
     return {
