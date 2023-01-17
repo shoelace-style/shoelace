e3e0842bdd7970d808cd4ca0fb55fb56e834c172
Cory LaViska <cory@abeautifulsite.net>
2023-01-17 10:44:07 -0500

    reorder importss

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,6 +1,6 @@
 import { esbuildPlugin } from '@web/dev-server-esbuild';
-import { playwrightLauncher } from '@web/test-runner-playwright';
 import { globbySync } from 'globby';
+import { playwrightLauncher } from '@web/test-runner-playwright';
 
 export default {
   rootDir: '.',
