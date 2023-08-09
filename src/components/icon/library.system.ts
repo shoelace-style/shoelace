c597924fb0a100b294af91219f9351d291f8da4c
Alexander Krolick <104371843+ak-beam@users.noreply.github.com>
2023-08-09 16:13:10 -0700

    feat: export system icons to allow individual overrides
    * Export the inline SVG icons from the system icon set so that 
      overriding just one of them is easier, and overrides are future- 
      proof (if a new one is added, it is used automatically).

--- src/components/icon/library.system.ts
+++ src/components/icon/library.system.ts
@@ -7,7 +7,7 @@ import type { IconLibrary } from './library.js';
 // All Shoelace components must use the system library instead of the default library. For visual consistency, system
 // icons are a subset of Bootstrap Icons.
 //
-const icons = {
+export const icons = {
   caret: `
     <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
       <polyline points="6 9 12 15 18 9"></polyline>
