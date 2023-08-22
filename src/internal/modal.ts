e9a008ae75d4afc3390559484131d69f23fa8e4f
konnorrogers <konnor5456@gmail.com>
2023-08-22 17:23:01 -0400

    prettier

--- src/internal/modal.ts
+++ src/internal/modal.ts
@@ -63,7 +63,6 @@ export default class Modal {
   startElementAlreadyFocused(startElement: HTMLElement) {
     for (const activeElement of activeElements()) {
       if (startElement === activeElement) {
-        //
         return true;
       }
     }
