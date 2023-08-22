0fdd25485df2b71863e6d3b9c95a2e9dbfe67127
konnorrogers <konnor5456@gmail.com>
2023-08-22 17:22:18 -0400

    add better note for generators

--- src/internal/active-elements.ts
+++ src/internal/active-elements.ts
@@ -1,10 +1,10 @@
 /**
  * Use a generator so we can iterate and possibly break early.
  * @example
- *   // to operate like a regular array. This gets kinda nullify generator benefits.
+ *   // to operate like a regular array. This kinda nullifies generator benefits, but worth knowing if you need the whole array.
  *   const allActiveElements = [...activeElements()]
  *
- *   // Earlier return
+ *   // Early return
  *   for (const activeElement of activeElements()) {
  *     if (<cond>) {
  *       break; // Break the loop, dont need to iterate over the whole array or store an array in memory!
