8d86f374f91d72e157ccc429e7754341bdd37f64
Burton Smith <burton@breakstuff.io>
2023-09-07 21:38:16 -0400

    clean up messaging

--- scripts/build.js
+++ scripts/build.js
@@ -215,7 +215,7 @@ await nextTask('Running the TypeScript compiler', () => {
 });
 
 // Copy the above steps to the CDN directory directly so we don't need to twice the work for nothing.
-await nextTask(`Copying Web Types, Themes, Icons, and TS Types to "${cdndir}"`, async () => {
+await nextTask(`Themes, Icons, and TS Types to "${cdndir}"`, async () => {
   await deleteAsync(cdndir);
   await copy(outdir, cdndir);
 });
