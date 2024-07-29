b8cdcd0c5566349f171545dbe8b7ea75e814cbe9
Konnor Rogers <konnor5456@gmail.com>
2024-07-29 11:21:04 -0400

    Fix submenu checkboxes (#2116)
    * fix submenu checkboxes not being checkable
    
    * add changelog entry
    
    * prettier
    
    * add changelog entry
    
    ---------
    
    Co-authored-by: Cory LaViska <cory@abeautifulsite.net>

--- src/components/menu/menu.component.ts
+++ src/components/menu/menu.component.ts
@@ -33,9 +33,16 @@ export default class SlMenu extends ShoelaceElement {
   private handleClick(event: MouseEvent) {
     const menuItemTypes = ['menuitem', 'menuitemcheckbox'];
 
-    const target = event.composedPath().find((el: Element) => menuItemTypes.includes(el?.getAttribute?.('role') || ''));
+    const composedPath = event.composedPath();
+    const target = composedPath.find((el: Element) => menuItemTypes.includes(el?.getAttribute?.('role') || ''));
 
     if (!target) return;
+
+    const closestMenu = composedPath.find((el: Element) => el?.getAttribute?.('role') === 'menu');
+    const clickHasSubmenu = closestMenu !== this;
+
+    // Make sure we're the menu thats supposed to be handling the click event.
+    if (clickHasSubmenu) return;
 
     // This isn't true. But we use it for TypeScript checks below.
     const item = target as SlMenuItem;
