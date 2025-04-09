39e3d69ef161e5a274deab89d51d8262610f1ce4
Sara <slhinyc@users.noreply.github.com>
2025-04-09 11:44:13 -0400

    Fix to display <sl-divider> again even with TW border resets

--- src/components/divider/divider.styles.ts
+++ src/components/divider/divider.styles.ts
@@ -9,14 +9,14 @@ export default css`
 
   :host(:not([vertical])) {
     display: block;
-    border-top: solid var(--width) var(--color);
+    border-top: solid var(--width) var(--color) !important; /* !important needed to override Tailwind's border resets */
     margin: var(--spacing) 0;
   }
 
   :host([vertical]) {
     display: inline-block;
     height: 100%;
-    border-left: solid var(--width) var(--color);
+    border-left: solid var(--width) var(--color) !important; /* !important needed to override Tailwind's border resets */
     margin: 0 var(--spacing);
   }
 `;
